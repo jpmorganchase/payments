31c25dbc12db95d2b9efd677a90d86442a2f9a37
Maria McParland <maria.mcparland94@gmail.com>
2021-03-29 11:00:08 +0100

    Removing console

--- src/store/apiStatusSlice.js
+++ src/store/apiStatusSlice.js
@@ -15,7 +15,5 @@ export default function apiStatusReducer(state = initialState, action) {
 // Thunk function
 export async function fetchApiStatus(dispatch) {
   const response = await client.get('v1/outages')
-  // eslint-disable-next-line no-console
-  console.log(response);
   dispatch({ type: 'apiStatus/apiStatusLoaded', payload: response.apiStatus })
 }
