e4b195e8a1728e5e4c457046e515bdf8d3d9f4d7
Maria McParland <maria.mcparland94@gmail.com>
2021-06-14 12:26:28 +0100

    Separating client and server

---
+++ server/index.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-console */
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.get("/api", (req, res) => {
+    res.json({ message: "Hello from server!" });
+});
+  
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
