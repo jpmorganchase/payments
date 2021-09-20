a9ebd38390e525a7e2948dcd8021eefa0192d59e
Maria McParland <maria.mcparland@icloud.com>
2021-09-20 13:11:30 +0100

    Adding error strings and certs to config module

--- server/config/index.js
+++ server/config/index.js
@@ -1,13 +1,19 @@
+require('dotenv').config();
+
 module.exports = {
   /**
    * Your favorite port
    */
   port: parseInt(process.env.PORT) || 5000,
+  env: process.env.NODE_ENV || 'development',
 
   /**
    * API configs
    */
   api: {
     prefix: '/api',
+    key: process.env.KEY && process.env.KEY.replace(/\\n/g, '\n'),
+    cert: process.env.CERT && process.env.CERT.replace(/\\n/g, '\n'),
+    errorString: 'Error hitting API',
   },
 };
--- server/dataController.js
+++ server/dataController.js
@@ -1,18 +1,10 @@
-require('dotenv').config();
-
 const path = require('path');
 const fs = require('fs');
 const https = require('https');
+const config = require('./config');
 
 const basePathToData = path.join(__dirname, 'mockJson');
 
-const key = process.env.KEY && process.env.KEY.replace(/\\n/g, '\n');
-const cert = process.env.CERT && process.env.CERT.replace(/\\n/g, '\n');
-
-// const key = fs.readFileSync(path.join(__dirname, '../unicorns/private.key'));
-//const cert = fs.readFileSync(path.join(__dirname, '../unicorns/unicorn.crt'));
-
-const errorString = 'Error hitting API';
 const getJsonData = function (basePathToData, filename) {
   var filename = path.join(basePathToData, filename);
   return JSON.parse(fs.readFileSync(filename, 'utf-8'));
@@ -22,7 +14,9 @@ function sendHttpsrequest(options) {
   return new Promise((resolve, reject) => {
     const req = https.request(options, (res) => {
       if (res.statusCode < 200 || res.statusCode >= 300) {
-        var error = new Error(`${errorString}, statusCode= ${res.statusCode}`);
+        var error = new Error(
+          `${config.errorString}, statusCode= ${res.statusCode}`,
+        );
         error.statusCode = res.statusCode;
         return reject(error);
       }
@@ -59,10 +53,10 @@ function handleHttpsRequest(response, options, mockedDataPath) {
       return response.status(200).send(JSON.stringify(result));
     })
     .catch((err) => {
-      if (err && err.message && err.message.includes(errorString)) {
+      if (err && err.message && err.message.includes(config.api.errorString)) {
         return response
           .status(err.statusCode)
-          .send(JSON.stringify({ errorString }));
+          .send(JSON.stringify(config.api.errorString));
       }
     });
 }
@@ -74,7 +68,7 @@ function isEmptyObject(value) {
 }
 
 function processData(options, response, mockedDataPath) {
-  if (process.env.NODE_ENV && process.env.NODE_ENV === 'production') {
+  if (config.env && config.env === 'production') {
     console.log(`Sending request to ${options.hostname}${options.path}`);
     return handleHttpsRequest(response, options, mockedDataPath);
   } else {
@@ -86,14 +80,13 @@ function processData(options, response, mockedDataPath) {
   }
 }
 exports.getServiceStatusData = function (request, response) {
-  console.log('here');
   const mockedDataPath = 'uf-service-status.json';
   const options = {
     hostname: 'apigatewayqaf.jpmorgan.com',
     path: '/tsapi/v1/outages',
     method: 'GET',
-    cert: cert,
-    key: key,
+    cert: config.api.cert,
+    key: config.api.key,
   };
   return processData(options, response, mockedDataPath);
 };
@@ -104,8 +97,8 @@ exports.getTransactionData = function (request, response) {
     hostname: 'apigatewayqaf.jpmorgan.com',
     path: '/tsapi/v2/transactions?accountIds=000000011116605&relativeDateType=PRIOR_DAY',
     method: 'GET',
-    cert: cert,
-    key: key,
+    cert: config.api.cert,
+    key: config.api.key,
   };
   return processData(options, response, mockedDataPath);
 };
@@ -124,8 +117,8 @@ exports.getBalanceData = function (request, response) {
     hostname: 'apigatewayqaf.jpmorgan.com',
     path: '/accessapi/balance',
     method: 'POST',
-    cert: cert,
-    key: key,
+    cert: config.api.cert,
+    key: config.api.key,
     headers: {
       'Content-Type': 'application/json',
       'Content-Length': postData.length,
