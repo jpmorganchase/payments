1ba931574538c3bfca6f690ce934cf55056fa1a3
Maria McParland <78078583+maria-mcparland@users.noreply.github.com>
2021-09-29 16:59:50 +0100

    Removing old changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 const ServiceStatusPage = lazy(() =>
   import('./components/pages/ServiceStatusPage/ServiceStatusPage'),
 );
@@ -7,18 +8,16 @@ const AccountPage = lazy(() =>
   import('./components/pages/AccountPage/AccountPage'),
 );
 
-const App = () => {
-  return (
-    <Router>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Switch>
-          <Route exact path='/' component={AccountPage} />
-          <Route exact path='/accounts' component={AccountPage} />
-          <Route path='/service_status' component={ServiceStatusPage} />
-        </Switch>
-      </Suspense>
-    </Router>
-  );
-};
+const App = () => (
+  <Router>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Switch>
+        <Route exact path='/' component={AccountPage} />
+        <Route exact path='/accounts' component={AccountPage} />
+        <Route path='/service_status' component={ServiceStatusPage} />
+      </Switch>
+    </Suspense>
+  </Router>
+);
 
 export default App;
