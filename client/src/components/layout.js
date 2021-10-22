5cca92221fd2855dfaf30e8f6df7f5399ddb877a
Maria McParland <78078583+maria-mcparland@users.noreply.github.com>
2021-10-22 15:51:09 +0100

    Mobile responsive

--- client/src/components/layout.js
+++ client/src/components/layout.js
@@ -7,8 +7,8 @@ const Layout = ({ children }) => {
     <div className='flex flex-col lg:flex-row h-full text-gray-900 min-h-screen'>
       <Sidebar />
       <section className='relative flex flex-col flex-grow'>
-        <main className='h-auto lg:h-full min-h-screen'>
-          <div className='content'>{children}</div>
+        <main className='h-auto lg:h-full lg:min-h-screen'>
+          <div className='content flex-wrap'>{children}</div>
         </main>
       </section>
     </div>
