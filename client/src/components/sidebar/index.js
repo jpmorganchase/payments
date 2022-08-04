ca940a5c7af390c720b2431a040f6864ed25b0ed
Maria McParland <78078583+maria-mcparland@users.noreply.github.com>
2022-08-04 09:55:07 +0100

    Upgrading navbar to v6 router and remove activeclassname

--- client/src/components/sidebar/index.js
+++ client/src/components/sidebar/index.js
@@ -4,6 +4,11 @@ import avatar from '../../images/avatar.png';
 import { NavLink } from 'react-router-dom';
 import { Popover } from '@headlessui/react';
 
+const linkClassName =
+  'border-t-2 lg:border-l-2 lg:border-t-0 border-transparent px-4 py-8 lg:py-2 lg:px-8 hover:text-gray-700';
+const activeClassName = 'border-pink-500 text-gray-900';
+const submenuClassName = 'block px-8 py-2 hover:text-gray-700 border-l-2';
+
 const Sidebar = () => {
   return (
     <div className='flex lg:flex-none w-full lg:w-44 border-b lg:border-r border-gray-200 '>
@@ -17,19 +22,15 @@ const Sidebar = () => {
             />
           </NavLink>
           <ul className=' ml-0 -my-4 lg:-ml-8 lg:my-0 text-gray-500 text-sm flex flex-row lg:flex-col'>
-            <li className='border-t-2 lg:border-l-2 lg:border-t-0 border-transparent'>
-              <a
-                href='#'
-                className='block px-4 py-8 lg:py-2 lg:px-8 hover:text-gray-700'
-              >
-                Dashboard
-              </a>
-            </li>
+            <NavLink to='#' className={linkClassName}>
+              <li className=''>Dashboard</li>
+            </NavLink>
 
             <NavLink
-              activeClassName='border-pink-500 text-gray-900'
               to='accounts'
-              className='border-t-2 lg:border-l-2 lg:border-t-0 border-transparent px-4 py-8 lg:py-2 lg:px-8 hover:text-gray-700'
+              className={({ isActive }) =>
+                isActive ? `${activeClassName} ${linkClassName}` : linkClassName
+              }
             >
               <li className=''>Accounts</li>
             </NavLink>
@@ -65,17 +66,18 @@ const Sidebar = () => {
 
           <ul className='hidden lg:block -ml-8 text-gray-500 text-xs mb-4'>
             <NavLink
-              activeClassName='border-pink-500 text-gray-900'
+              className={({ isActive }) =>
+                isActive
+                  ? `${activeClassName} ${submenuClassName}`
+                  : submenuClassName
+              }
               to='service_status'
-              className='block px-8 py-2 hover:text-gray-700 border-l-2'
             >
               <li>Service status</li>
             </NavLink>
-            <li className='border-l-2 border-transparent'>
-              <a className='block px-8 py-2 hover:text-gray-700' href='#'>
-                Settings
-              </a>
-            </li>
+            <NavLink className={submenuClassName} to='service_status'>
+              <li>Settings</li>
+            </NavLink>
           </ul>
           <div className='hidden lg:flex text-sm relative'>
             <span className='block absolute bg-red-500 p-1 left-10 -top-1 rounded-xl'></span>
