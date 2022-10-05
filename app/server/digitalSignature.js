1f88ea726dd2de69a7ccaded669d8e0a4f07b88c
Maria McParland <78078583+maria-mcparland@users.noreply.github.com>
2022-10-05 11:52:20 +0100

    Extracting body to file for ease

--- app/server/digitalSignature.js
+++ app/server/digitalSignature.js
@@ -1,66 +1,7 @@
 const fs = require('fs');
 const jose = require('jose');
+const { body } = require('./body');
 
-const body = {
-  paymentMethod: 'TRF',
-  paymentTypeInformation: {
-    serviceLevelCode: 'NURG',
-    localInstrument: {
-      code: 'CCD',
-    },
-  },
-  requestedExecutionDate: '2022-11-11',
-  debtor: {
-    name: 'Test Name',
-    postalAddress: {
-      country: 'US',
-    },
-    organizationId: {
-      other: [
-        {
-          id: '4192662001',
-          schemeName: {
-            proprietary: 'JPMCOID',
-          },
-        },
-      ],
-    },
-  },
-  debtorAccount: {
-    accountNumber: '945958726',
-    currency: 'USD',
-  },
-  debtorAgent: {
-    aba: '021000021',
-    country: 'US',
-  },
-  creditTransferTransactionInformation: {
-    endToEndId: 'E2E006456',
-    amount: {
-      instructedAmount: {
-        currency: 'USD',
-        amount: 101.39,
-      },
-    },
-    creditorAgent: {
-      financialInstitutionId: {
-        aba: '021000021',
-        postalAddress: {
-          country: 'US',
-        },
-      },
-    },
-    creditor: {
-      name: 'SAMPLE CREDITOR',
-      postalAddress: {
-        country: 'US',
-      },
-    },
-    creditorAccount: {
-      accountNumber: '945958759',
-    },
-  },
-};
 const digitalSignatureKey = fs.readFileSync(
   'certs/treasury-services/digital-signature/key.key',
   'utf-8',
