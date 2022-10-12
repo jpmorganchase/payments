976e8ca5460a0c5171cb086a5cefdd985769015a
Maria McParland <78078583+maria-mcparland@users.noreply.github.com>
2022-10-12 12:36:03 +0100

    Simplify layout.js

--- app/client/src/components/layout.js
+++ app/client/src/components/layout.js
@@ -6,13 +6,9 @@ const Layout = () => {
   return (
     <div className='flex flex-col lg:flex-row h-full text-gray-900 min-h-screen'>
       <Sidebar />
-      <section className='relative flex flex-col flex-grow'>
-        <main className='h-auto lg:h-full lg:min-h-screen'>
-          <div className='content flex-wrap'>
-            <Outlet />
-          </div>
-        </main>
-      </section>
+      <main className='h-auto lg:h-full lg:min-h-screen flex-grow'>
+        <Outlet />
+      </main>
     </div>
   );
 };
