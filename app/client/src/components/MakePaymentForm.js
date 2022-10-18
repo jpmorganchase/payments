56d0614b0cf574117869b2800a9a4de2dff6c04f
Maria McParland <78078583+maria-mcparland@users.noreply.github.com>
2022-10-18 14:48:42 +0100

    Basic input fields added

--- app/client/src/components/MakePaymentForm.js
+++ app/client/src/components/MakePaymentForm.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const renderInputField = (label, id, type = 'text') => (
+  <div className=''>
+    <label htmlFor={id} className='block text-sm font-medium text-gray-700'>
+      {label}:
+    </label>
+    <input
+      type={type}
+      name={id}
+      className='mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm'
+    />
+  </div>
+);
 const MakePaymentForm = ({ closeModal }) => {
   const submitPayment = () => {
     closeModal();
@@ -8,7 +20,11 @@ const MakePaymentForm = ({ closeModal }) => {
 
   return (
     <form>
-      hello
+      {renderInputField('From', 'debtorAccountId')}
+      {renderInputField('To', 'creditorAccountId')}
+      {renderInputField('Bank ID', 'creditorAgent')}
+      {renderInputField('Amount', 'amount', 'number')}
+      {renderInputField('Date', 'date', 'date')}
       <button
         className='p-1 bg-gradient-to-r from-pink-500 to-red-500  font-medium rounded-lg text-white text-center flex items-center justify-center'
         onClick={submitPayment}
--- app/client/src/components/search.js
+++ app/client/src/components/search.js
@@ -2,10 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Search = ({ searchInput, setSearchInput, searchText, testingId }) => (
-  <div className='bg-gray-100 hover:bg-gray-200 cursor-pointer text-xs rounded-lg px-2 py-1 flex items-center'>
+  <div className='bg-gray-100 hover:bg-gray-200 cursor-pointer text-xs rounded-lg px-2 py-1 flex items-center border-0'>
     <span className='material-icons text-base mr-1'>search</span>
     <input
-      className='bg-gray-100 hover:bg-gray-200'
+      className='bg-gray-100 hover:bg-gray-200 border-0 focus:ring-0'
       type='search'
       placeholder={searchText}
       onChange={(e) => setSearchInput(e.target.value)}
