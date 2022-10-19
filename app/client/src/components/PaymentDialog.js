c66ad7dfd2086d55976a142e4824e11c6d895437
Maria McParland <78078583+maria-mcparland@users.noreply.github.com>
2022-10-19 15:48:32 +0100

    What api added to dialog

--- app/client/src/components/PaymentDialog.js
+++ app/client/src/components/PaymentDialog.js
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react';
 import { Dialog, Transition } from '@headlessui/react';
 import MakePaymentForm from './MakePaymentForm';
 import PropTypes from 'prop-types';
+import { AppContext } from '../AppContext';
 const { config } = require('../config');
 
 const PaymentDialog = ({ isPaymentFormOpen, setPaymentFormOpen, ...props }) => {
@@ -9,7 +10,8 @@ const PaymentDialog = ({ isPaymentFormOpen, setPaymentFormOpen, ...props }) => {
     setPaymentFormOpen(false);
   }
   const { paymentConfig } = config;
-  console.log(paymentConfig);
+  const { displayingApiData } = React.useContext(AppContext);
+
   return (
     <>
       <Transition appear show={isPaymentFormOpen} as={Fragment}>
@@ -37,18 +39,42 @@ const PaymentDialog = ({ isPaymentFormOpen, setPaymentFormOpen, ...props }) => {
                 leaveFrom='opacity-100 scale-100'
                 leaveTo='opacity-0 scale-95'
               >
-                <Dialog.Panel className='w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all'>
-                  <Dialog.Title
-                    as='h3'
-                    className='text-lg font-medium leading-6 text-gray-900 flex justify-between'
-                  >
-                    Make a payment
-                    <button onClick={closeModal} data-cy='closeButton'>
-                      <span className='material-icons text-md mr-1'>close</span>
-                    </button>
-                  </Dialog.Title>
-                  <MakePaymentForm closeModal={closeModal} {...props} />
-                </Dialog.Panel>
+                {displayingApiData ? (
+                  <Dialog.Panel className='w-full max-w-md transform overflow-hidden rounded-2xl p-6 text-left align-middle shadow-xl transition-all bg-black text-white'>
+                    <Dialog.Title
+                      as='h3'
+                      className='text-lg font-medium leading-6 flex justify-between'
+                    >
+                      {paymentConfig.apiDetails[0].name} API
+                      <button onClick={closeModal} data-cy='closeButton'>
+                        <span className='material-icons text-md mr-1'>
+                          close
+                        </span>
+                      </button>
+                    </Dialog.Title>
+                    <h3 className='text-xs mb-4'>
+                      {paymentConfig.apiDetails[0].path}
+                    </h3>
+                    <h3 className='text-xs'>
+                      {paymentConfig.apiDetails[0].description}
+                    </h3>
+                  </Dialog.Panel>
+                ) : (
+                  <Dialog.Panel className='w-full max-w-md transform overflow-hidden rounded-2xl p-6 text-left align-middle shadow-xl transition-all bg-white'>
+                    <Dialog.Title
+                      as='h3'
+                      className='text-lg font-medium leading-6 text-gray-900 flex justify-between'
+                    >
+                      Make a payment
+                      <button onClick={closeModal} data-cy='closeButton'>
+                        <span className='material-icons text-md mr-1'>
+                          close
+                        </span>
+                      </button>
+                    </Dialog.Title>
+                    <MakePaymentForm closeModal={closeModal} {...props} />
+                  </Dialog.Panel>
+                )}
               </Transition.Child>
             </div>
           </div>
