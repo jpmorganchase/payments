e1250d094f394c8a00cf33baa4caaac4fd18cb88
Maria McParland <78078583+maria-mcparland@users.noreply.github.com>
2022-10-25 11:12:13 +0100

    Some basic form validation

--- app/client/src/components/MakePaymentForm.js
+++ app/client/src/components/MakePaymentForm.js
@@ -3,7 +3,53 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useForm } from 'react-hook-form';
 
-const renderSelectField = (label, id, options, register, addOption = false) => (
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
+const patternTwoDigisAfterDot = /^\d+(\.\d{0,2})?$/;
+const today = new Date();
+const oneMonth = new Date(new Date(today).setDate(today.getDate() + 31))
+  .toISOString()
+  .split('T')[0];
+
+const schema = yup
+  .object({
+    debtorAccountId: yup.string().required(),
+    creditorAccountId: yup.string().required(),
+
+    date: yup
+      .date()
+      .typeError('Please enter a valid date')
+      .min(today.toISOString().split('T')[0], 'Date cannot be in the past')
+      .max(oneMonth, 'Date cannot be more than 30days in advance')
+      .required(),
+    amount: yup
+      .number()
+      .typeError('Amount is required')
+
+      .positive()
+      .test(
+        'is-decimal',
+        'The amount should be a decimal with maximum two digits',
+        (val) => {
+          if (val != undefined) {
+            return patternTwoDigisAfterDot.test(val);
+          }
+          return true;
+        },
+      )
+      .required('Is required'),
+  })
+  .required();
+
+const renderSelectField = (
+  label,
+  id,
+  options,
+  register,
+  errors,
+  addOption = false,
+) => (
   <div className='col-span-6 sm:col-span-3'>
     <label htmlFor={id} className='block text-sm font-medium text-gray-700'>
       {label}:
@@ -26,6 +72,7 @@ const renderSelectField = (label, id, options, register, addOption = false) => (
       ))}
       {addOption && <option key='option-add'>Add new account details</option>}
     </select>
+    <p>{errors.id?.message}</p>
   </div>
 );
 
@@ -40,7 +87,9 @@ const MakePaymentForm = ({ closeModal, data }) => {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm();
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
 
   const onSubmit = (data) => {
     console.log(data);
@@ -55,12 +104,14 @@ const MakePaymentForm = ({ closeModal, data }) => {
           'debtorAccountId',
           data?.accountList,
           register,
+          errors,
         )}
         {renderSelectField(
           'To',
           'creditorAccountId',
           data?.accountList,
           register,
+          errors,
           true,
         )}
         <div className=''>
@@ -77,6 +128,7 @@ const MakePaymentForm = ({ closeModal, data }) => {
             step='0.01'
             className='mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm'
           />
+          <p>{errors.amount?.message}</p>
         </div>
         <div className=''>
           <label
@@ -89,8 +141,10 @@ const MakePaymentForm = ({ closeModal, data }) => {
             {...register('date', { valueAsDate: true })}
             type='date'
             name='date'
+            min={today.toISOString().split('T')[0]}
             className='mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm'
           />
+          <p>{errors.date?.message}</p>
         </div>
 
         <button className='p-1 bg-gradient-to-r from-pink-500 to-red-500  font-medium rounded-lg text-white text-center flex items-center justify-center'>
