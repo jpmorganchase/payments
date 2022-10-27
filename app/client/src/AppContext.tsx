82d342a062368cda0166677155fbf230b63a78ac
Maria McParland <78078583+maria-mcparland@users.noreply.github.com>
2022-10-27 09:47:49 +0100

    Converted app context to typescript

---
+++ app/client/src/AppContext.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+
+interface AppContextInterface {
+  setDisplayingMockedData: (displayingMockedData: boolean) => void,
+  setDisplayingApiData: (displayingApiData:boolean) => void,
+  displayingApiData: boolean,
+  displayingMockedData: boolean,
+}
+
+interface Props {
+  children?: React.ReactNode
+}
+
+const appCtxDefaultValue: AppContextInterface = {
+  displayingApiData: false,
+  displayingMockedData: true,
+  setDisplayingApiData: () => {},
+  setDisplayingMockedData: () => {}
+};
+
+const AppContext = React.createContext<AppContextInterface>( appCtxDefaultValue);
+
+const AppContextProvider = ({ children }: Props) => {
+  const [displayingMockedData, setDisplayingMockedData] = React.useState(appCtxDefaultValue.displayingMockedData);
+  const [displayingApiData, setDisplayingApiData] = React.useState(appCtxDefaultValue.displayingApiData);
+
+  return (
+    <AppContext.Provider
+      value={{
+        displayingMockedData,
+        setDisplayingMockedData,
+        displayingApiData,
+        setDisplayingApiData,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export { AppContextProvider, AppContext };
