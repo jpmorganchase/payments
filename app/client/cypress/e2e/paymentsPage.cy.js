d60e9a8096ee580ad8e4ac23b29ac9c1c001cb8e
Maria McParland <78078583+maria-mcparland@users.noreply.github.com>
2022-12-05 14:06:53 +0000

    Removing old validation tests

--- app/client/cypress/e2e/paymentsPage.cy.js
+++ app/client/cypress/e2e/paymentsPage.cy.js
@@ -13,31 +13,11 @@ describe('Payments page', () => {
     const oneMonthAndADay = new Date(new Date(today).setDate(today.getDate() + 32)).toISOString()
     .split('T')[0];
   
-    it('Assert you can\'t enter negative amount and more than two decimals', () => {
-      cy.contains('Send a Payment');
-      cy.get('[data-cy="amount"]').type('-1');
-      cy.contains('amount must be a positive number');
-      cy.get('[data-cy="amount"]').clear();
-      cy.get('[data-cy="amount"]').type('0.234');
-      cy.contains('The amount should be a decimal with maximum two digits');
-      cy.get('[data-cy="amount"]').clear();
-    });
-
-    it('Assert you can\'t enter today\s date or day 30+ days in future', () => {
-      cy.get('[data-cy="dateInput"]').type(oneMonthAndADay);
-      cy.contains('Date cannot be more than 30days in advance');
-      cy.get('[data-cy="dateInput"]').clear();
-      cy.contains('Please enter a valid date');
-      cy.get('[data-cy="dateInput"]').type(oneMonth);
-      cy.contains('Date cannot be more than 30days in advance').should('not.exist');
-      cy.get('[data-cy="dateInput"]').clear();
-      });
-
     it('Previous JSON button selected', () => {
       viewports.forEach((viewport) => {
         cy.viewport(viewport);
         cy.get('[data-cy="amount"]').type('123456789');
-        cy.get('[data-cy="dateInput"]').type(today.toISOString().split('T')[0]);
+        cy.get('[data-cy="date"]').type(today.toISOString().split('T')[0]);
 
         cy.contains('Preview JSON')
           .click();
@@ -51,7 +31,7 @@ describe('Payments page', () => {
 
     it('Submit button selected', () => {
       cy.get('[data-cy="amount"]').type('1000');
-      cy.get('[data-cy="dateInput"]').type(today.toISOString().split('T')[0]);
+      cy.get('[data-cy="date"]').type(today.toISOString().split('T')[0]);
       cy.contains('Submit')
       .click();
       cy.contains('Success!');
