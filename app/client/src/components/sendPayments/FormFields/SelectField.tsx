c2cbd880b496b85f8df46629e9cb92834accfc68
Maria McParland <78078583+maria-mcparland@users.noreply.github.com>
2022-12-06 16:02:03 +0000

    Added in more tests for payment form

--- app/client/src/components/sendPayments/FormFields/SelectField.tsx
+++ app/client/src/components/sendPayments/FormFields/SelectField.tsx
@@ -47,6 +47,7 @@ export default function SelectField({
         :
       </label>
       <select
+        data-cy={id}
         {...register(id)}
         defaultValue={defaultValue}
         className="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
