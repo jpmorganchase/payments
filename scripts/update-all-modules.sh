784be75e26c3d5168dedb50a908ac82a898d96f1
Maria McParland <78078583+maria-mcparland@users.noreply.github.com>
2023-02-01 11:43:07 +0000

    Update all submodules

--- scripts/update-all-modules.sh
+++ scripts/update-all-modules.sh
@@ -3,6 +3,7 @@
 current_date_time="`date +%Y%m%d%H%M%S`";
 echo $current_date_time;
 
+git checkout main
 git checkout -b "update_sub_modules-${current_date_time}" main
 git submodule foreach git pull
 git commit -am "Update all submodules"
